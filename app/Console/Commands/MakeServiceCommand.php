708117230bc8f5e1e797380a74b666360d111343
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-03-31 22:12:45 -0300

    Add custom Artisan command for service class and various stubs

---
+++ app/Console/Commands/MakeServiceCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeServiceCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    public function getStub()
+    {
+        return __DIR__ . '/../../../stubs/service.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Services';
+    }
+}
